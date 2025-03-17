7cf24ae626fea85cad4eb8219d7854ccba86493c
Joe Wright <joewrightwebdev@gmail.com>
2025-03-17 11:41:03 -0400

    engineering practical ready

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Laravel Starter</title>
+</head>
+<body>
+    <h1>Weather Dashboard</h1>
+    <p>This is a starting point for your assignment.</p>
+    <div>
+        <p>Current Temperature: <span>XX</span></p>
+        <p>Current Condition: Sunny</p>
+    </div>
+    <div>
+        <h2>Additional Details</h2>
+        <p>Humidity: <span>X%</span></p>
+        <p>Wind Speed: <span>X</span></p>
+    </div>
+</body>
+</html>
